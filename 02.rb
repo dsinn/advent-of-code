4b3703c721270cf8c39c4cf1b7cb7a67a58fd186
Daniel Sinn <daniel.p.sinn@gmail.com>
2020-12-02 00:43:17 -0500

    [02]

---
+++ 02.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+filename = '02.txt'
+raise StandardError.new "#{filename} does not exist in the current working directory." unless File.file? filename
+
+part1_count = 0
+part2_count = 0
+File.open(filename, 'r').each_line do |line|
+  matches = /^(\d+)-(\d+) (.): (.+)$/.match(line)
+  raise StandardError.new "Could not match #{line}" unless matches
+
+  _, _, _, letter, password = matches.to_a
+  number1 = matches[1].to_i
+  number2 = matches[2].to_i
+
+  letter_count = password.scan(letter).count
+  part1_count += 1 if number1 <= letter_count && letter_count <= number2
+  part2_count += 1 if (password[number1 - 1] == letter) != (password[number2 - 1] == letter)
+end
+
+puts "Part 1: #{part1_count}"
+puts "Part 2: #{part2_count}"
