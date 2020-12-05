c16a5d8189d4bb1e24771a814fc0d4ce729cedb5
Daniel Sinn <daniel.p.sinn@gmail.com>
2020-12-05 00:51:47 -0500

    [05] Okay fine, switch to O(n)

--- 2020/05.rb
+++ 2020/05.rb
@@ -1,21 +1,21 @@
 #!/usr/bin/env ruby
 require 'set'
 
-seats = SortedSet.new
-max = 0 # Set doesn't have a `.last`, oof
+min = Float::INFINITY
+max = 0
+seats = {} # Existence map
 File.open('05.txt', 'r').each_line.each do |line|
   row = line[0..6].gsub(/[FB]/, 'F' => '0', 'B' => '1').to_i(2)
   column = line[7..-1].gsub(/[LR]/, 'L' => '0', 'R' => '1').to_i(2)
 
   seat = row * 8 + column
-  seats << seat
+  seats[seat] = true
+
+  min = seat if min > seat
   max = seat if max < seat
 end
 
 puts "Highest seat number: #{max}"
 
-previous_seat = seats.first # Hack-ish since I can't start iterating on the second element of a Set
-seats.each do |seat|
-  puts "Your seat could be between seats #{previous_seat} and #{seat}." if seat - previous_seat > 1
-  previous_seat = seat
-end
+print 'Missing seats: '
+puts (min + 1..max - 1).reject {|i| seats.has_key? i}.inspect
