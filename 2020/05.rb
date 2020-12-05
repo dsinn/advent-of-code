517e7691800565ee1ee143f658c9fa8c7d74f74e
Daniel Sinn <daniel.p.sinn@gmail.com>
2020-12-05 01:13:29 -0500

    [05] Draw a map for funsies
    Thanks to @YoungBK for optimizing the seat number.

--- 2020/05.rb
+++ 2020/05.rb
@@ -5,17 +5,27 @@ min = Float::INFINITY
 max = 0
 seats = {} # Existence map
 File.open('05.txt', 'r').each_line.each do |line|
-  row = line[0..6].gsub(/[FB]/, 'F' => '0', 'B' => '1').to_i(2)
-  column = line[7..-1].gsub(/[LR]/, 'L' => '0', 'R' => '1').to_i(2)
-
-  seat = row * 8 + column
+  seat = line.gsub(/[FBLR]/, 'F' => '0', 'B' => '1', 'L' => '0', 'R' => '1').to_i(2)
   seats[seat] = true
 
   min = seat if min > seat
   max = seat if max < seat
 end
 
-puts "Highest seat number: #{max}"
+missing_seats = []
+for i in 0..max
+  if i < min
+    print ' '
+  elsif seats.has_key?(i)
+    print '.'
+  else
+    print 'X'
+    missing_seats << i
+  end
+  print ' ' if i % 4 === 3
+  puts " #{(i - 7).to_s.rjust(3)}-#{i.to_s.rjust(3)}" if i % 8 === 7
+end
+puts ''
 
-print 'Missing seats: '
-puts (min + 1..max - 1).reject {|i| seats.has_key? i}.inspect
+puts "Highest seat number: #{max}"
+puts "Missing seats: #{missing_seats.inspect}"
