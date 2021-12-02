a059808379ae89920bc883e4352e39a0e183fe67
Daniel Sinn <daniel.p.sinn@gmail.com>
2021-12-02 00:49:02 -0500

    [02]

---
+++ 2021/02.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+f = open('02.txt', 'r')
+
+positions = [{'horizontal': 0, 'depth': 0} for _ in range(2)]
+aim = 0
+
+for line in f.readlines():
+    direction, magnitude = line.split(' ')
+    magnitude = int(magnitude)
+    match direction:
+        case 'forward':
+            for position in positions:
+                position['horizontal'] += magnitude
+            positions[1]['depth'] += aim * magnitude
+        case 'up':
+            positions[0]['depth'] -= magnitude
+            aim -= magnitude
+        case 'down':
+            positions[0]['depth'] += magnitude
+            aim += magnitude
+        case _:
+            raise ValueError(f'"{direction}" is not a recognized direction string.')
+
+for i, position in enumerate(positions):
+    print(f'Part {i + 1}: {position["horizontal"] * position["depth"]}')
