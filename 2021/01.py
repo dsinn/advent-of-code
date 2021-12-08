282636e37d3cc3a9060f692676bc44970c1879c9
Daniel Sinn <daniel.p.sinn@gmail.com>
2021-12-08 11:15:56 -0500

    [01] Optimize it a bit
    Thanks to Bill.

--- 2021/01.py
+++ 2021/01.py
@@ -8,7 +8,6 @@ f = open('01.txt', 'r')
 
 # Initialize the sliding window so that the main loop is simpler
 previous_numbers = list(map(lambda x: int(x.rstrip()), [f.readline() for x in range(WINDOW_SIZE)]))
-previous_sum = sum(previous_numbers)
 
 # ...with the trade-off being having to compensate for Part 1 here.
 for i in range(1, len(previous_numbers)):
@@ -21,17 +20,13 @@ while True:
         break
 
     number = int(line.rstrip())
-    # The window size is too small to optimize the summation like this, but...shut up.
-    current_sum = previous_sum + number - previous_numbers[0]
-
-    if current_sum > previous_sum:
+    if number > previous_numbers[0]:
         sliding_window_increase_count += 1
     if number > previous_numbers[-1]:
         singular_increase_count += 1
 
     previous_numbers.pop(0)
     previous_numbers.append(number)
-    previous_sum = current_sum
 
 print(f'Part 1: {singular_increase_count}')
 print(f'Part 2: {sliding_window_increase_count}')
