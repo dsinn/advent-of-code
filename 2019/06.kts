6920c07cf4d3ceba90f993b55b82e7f23a1e5712
Daniel Sinn <daniel.p.sinn@gmail.com>
2022-07-06 12:52:16 -0400

    [2019#6] Memoize part 1 to not run in O(n^2) time

--- 2019/06.kts
+++ 2019/06.kts
@@ -3,19 +3,23 @@
 import java.io.File
 
 // orbiter -> orbitee
-val orbits = File("06.txt").readLines().associate { line -> line.split(")").let { it[1] to it[0] } }
+val orbits = File("06.txt").readLines().associate { line -> line.split(")").let { obj -> obj[1] to obj[0] } }
 
-val orbitCounts: MutableMap<String, Int> = mutableMapOf()
+fun computeDistanceFromCentre (orbiter: String, distances: MutableMap<String, Int>, orbits: Map<String, String>): Int {
+    return distances.getOrPut(orbiter) {
+        if (!orbits.containsKey(orbiter)) {
+            // This is the centre (COM)
+            return@getOrPut 0
+        }
 
-orbits.forEach { (orbiter, orbitee) ->
-    var suborbiter = orbiter
-    while (orbits.containsKey(suborbiter)) {
-        orbitCounts[orbitee] = orbitCounts.getOrPut(orbitee, { 0 }) + 1
-        suborbiter = orbits.getValue(suborbiter)
+        val orbitee = orbits.getValue(orbiter)
+        computeDistanceFromCentre(orbitee, distances, orbits) + 1
     }
 }
 
-println("Part 1: ${orbitCounts.values.sum()}")
+val distances: MutableMap<String, Int> = mutableMapOf()
+orbits.keys.forEach { computeDistanceFromCentre(it, distances, orbits) }
+println("Part 1: ${distances.values.sum()}")
 
 // Since the system is a single tree, we can compute the number of orbital transfers based on the point at which their
 // paths to the root (COM) join.
