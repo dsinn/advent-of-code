62ff720377bd13954900f3c7055c8f339eb1ea93
Daniel Sinn <daniel.p.sinn@gmail.com>
2023-12-03 04:08:42 -0500

    [03] One exec_all per gear instead of three

--- 2023/03.ml
+++ 2023/03.ml
@@ -40,40 +40,42 @@ let () =
 ;;
 
 let () =
+  let number_rex = Pcre.regexp "\\d+" in
   Pcre.exec_all ~pat:"\\*" schematic
   |> Array.fold_left
        (fun sum substring ->
          let gear_offset = Pcre.get_substring_ofs substring 0 |> fst in
          let gear_column = gear_offset mod width in
          let line_start_offset = gear_offset - gear_column in
-         [ line_start_offset - width; line_start_offset; line_start_offset + width ]
-         |> List.filter (fun pos -> pos >= 0 && pos < String.length schematic)
-         |> List.map (fun pos -> String.sub schematic pos (width - 1))
-         |> List.map (fun line ->
-           try
-             Pcre.exec_all ~pat:"\\d+" line
-             |> Array.fold_left
-                  (fun adjacent_numbers substring ->
-                    let number_string = Pcre.get_substring substring 0 in
-                    let length = String.length number_string in
-                    let offset = Pcre.get_substring_ofs substring 0 |> fst in
-                    let leftmost_column = offset mod width in
-                    let rightmost_column = leftmost_column + length - 1 in
-                    if Int.abs (leftmost_column - gear_column) < 2
-                       || Int.abs (rightmost_column - gear_column) < 2
-                       || (leftmost_column < gear_column && rightmost_column > gear_column)
-                    then int_of_string number_string :: adjacent_numbers
-                    else adjacent_numbers)
-                  []
-           with
-           | Not_found -> [])
-         |> List.flatten
+         let search_start_pos = Int.max 0 (line_start_offset - width) in
+         let search_end_pos =
+           Int.min (String.length schematic) (line_start_offset + (2 * width)) - 1
+         in
+         let search_string =
+           String.sub schematic search_start_pos (search_end_pos - search_start_pos)
+         in
+         (try
+            Pcre.exec_all ~rex:number_rex search_string
+            |> Array.fold_left
+                 (fun adjacent_numbers substring ->
+                   let number_string = Pcre.get_substring substring 0 in
+                   let length = String.length number_string in
+                   let offset = Pcre.get_substring_ofs substring 0 |> fst in
+                   let leftmost_column = offset mod width in
+                   let rightmost_column = leftmost_column + length - 1 in
+                   if Int.abs (leftmost_column - gear_column) < 2
+                      || Int.abs (rightmost_column - gear_column) < 2
+                      || (leftmost_column < gear_column && rightmost_column > gear_column)
+                   then int_of_string number_string :: adjacent_numbers
+                   else adjacent_numbers)
+                 []
+          with
+          | Not_found -> [])
          |> yield_self (fun adjacent_numbers ->
-           sum
-           +
            if List.length adjacent_numbers = 2
            then List.reduce ( * ) adjacent_numbers
-           else 0))
+           else 0)
+         |> ( + ) sum)
        0
   |> Printf.printf "Part 2: %d\n"
 ;;
