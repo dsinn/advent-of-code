a1414bddf0c491f6f5e45b0453b68ddc9ca480d6
Daniel Sinn <daniel.p.sinn@gmail.com>
2023-12-09 00:56:03 -0500

    [09] I considered Lagrange polynomials...but no

---
+++ 2023/09.ml
@@ -0,0 +1,43 @@
+#!/usr/bin/env -S opam exec -- ocaml
+
+#use "helpers.ml"
+
+open Str
+
+let rows_diffs =
+  let rec pair_map f list =
+    match list with
+    | x :: y :: rest -> f x y :: pair_map f (y :: rest)
+    | _ -> []
+  in
+  let rec calc_diffs row diffs =
+    let next_row = pair_map (fun x y -> y - x) row in
+    if List.for_all (( = ) 0) next_row
+    then diffs
+    else calc_diffs next_row (diffs @ [ next_row ])
+  in
+  let history =
+    File.lines_of "09.txt"
+    |> Enum.map (fun line -> Str.split (Str.regexp " +") line |> List.map int_of_string)
+    |> List.of_enum
+  in
+  List.map (fun row -> calc_diffs row [ row ]) history
+;;
+
+let sum_extrapolees rows_diffs f_extrapolee =
+  List.fold_left
+    (fun sum row_diffs ->
+      List.rev row_diffs |> List.fold_left f_extrapolee 0 |> ( + ) sum)
+    0
+    rows_diffs
+;;
+
+let () =
+  sum_extrapolees rows_diffs (fun extrapolee diff -> List.last diff + extrapolee)
+  |> Printf.printf "Part 1: %d\n"
+;;
+
+let () =
+  sum_extrapolees rows_diffs (fun extrapolee diff -> List.hd diff - extrapolee)
+  |> Printf.printf "Part 2: %d\n"
+;;
